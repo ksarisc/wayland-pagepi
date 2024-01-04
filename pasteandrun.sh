bfcaa0b681ba53a5e601a6c096b1677bdbcd9d1f
Ugotapi <rudischmitz@gmail.com>
2024-01-04 16:03:28 -0600

    Update pasteandrun.sh

--- pasteandrun.sh
+++ pasteandrun.sh
@@ -16,6 +16,7 @@ sudo chmod +x /usr/bin/hideaway
 
 wget https://raw.githubusercontent.com/ugotapi/wayland-pagepi/main/config.yaml
 sudo cp /home/$USER/config.yaml /etc/interception/udevmon.d/config.yaml
+sudo systemctl restart udevmon
 
 
 
