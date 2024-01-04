e8e90ab9f416e34178a54309d290ed05c135650c
Ugotapi <rudischmitz@gmail.com>
2024-01-04 13:47:49 -0600

    Update pasteandrun.sh

--- pasteandrun.sh
+++ pasteandrun.sh
@@ -30,7 +30,7 @@ EOFZ
 cat > /home/$USER/myscript.sh << EOL
 #!/bin/sh
 # what this script does: start chromium
-chromium-browser --start-maximized  --incognito --user-data-dir=/home/$USER/.config/chromium2 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter --app=https://apple.com &
+chromium-browser --start-maximized  --incognito --user-data-dir=/home/$USER/.config/chromium2 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter --app=https://twitch.tv &
 EOL
 
 sudo chmod +x /home/$USER/myscript.sh
